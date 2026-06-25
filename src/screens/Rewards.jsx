93332554bbefbf5f47a0a691bc6a9cc6aafbcb8f
Gbemiga02 <olugbodigbemiga@gmail.com>
2026-06-25 06:52:47 -0700

    update on reward screen

--- src/screens/Rewards.jsx
+++ src/screens/Rewards.jsx
@@ -1,143 +1,125 @@
-import { StyleSheet, Text, View, Modal, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, Modal, TouchableOpacity, Image, FlatList } from 'react-native'
 import { use, useState } from 'react';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-
+import Entypo from '@expo/vector-icons/Entypo';
+import EvilIcons from '@expo/vector-icons/EvilIcons';
 export default function Rewards() {
 
   const [isModalOn, setIsModalOn] = useState(false);
+  const Data = [
+    {
+      id: 1,
+      image: require('../assets/icons8-friends-100.png'),
+      title: 'Friday Bouns'
+    },
+    {
+      id: 2,
+      image: require('../assets/icons8-money-100.png'),
+      title: 'Referral Friends'
+    },
+    {
+      id: 3,
+      image: require('../assets/icons8-star-100.png'),
+      title: 'Play4achild'
+    },
+    {
+      id: 4,
+      image: require('../assets/icons8-voucher-100 (1).png'),
+      title: 'Voucher Pack'
+    },
+  ]
+
+  const renderItem = ({ item }) => (
+    <View style={{ marginLeft:10 }}>
+      <View style={{ height: 80, width: 80, borderRadius: 5, backgroundColor: '#383737', alignItems:'center', justifyContent:'center' }}>
+        
+        <View  style={{alignItems:'center', justifyContent:'center', }}>
+          <View style={{height:35, width:35, backgroundColor:'#393535', alignItems:'center', justifyContent:'center', borderRadius:5}}>
+        <Image source={item.image} style={{ height: 30, width: 30, }} />
+          </View>
+        <Text style={{ color: 'white', fontSize:10 }}>{item.title}</Text>
+        </View>
+      </View>
+    </View>
+  )
 
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
+        <View style={{ height: 200, width: "100%", backgroundColor: '#0c542c', padding: 20, borderBottomRightRadius:20, borderBottomLeftRadius:20 }}>
+          <View style={{ flexDirection: "row", alignItems: 'center', gap: 10, justifyContent: 'space-between' }}>
+            <Text style={{ fontSize: 20, color: 'white' }}>
+              Rewards
+            </Text>
+            <View style={{ width: 30, height: 30, borderRadius: 30, borderWidth: 2, justifyContent: 'center', alignItems: 'center', borderColor: 'white' }}>
+              <Entypo name="dots-three-horizontal" size={18} color="white" />
+            </View>
+          </View>
 
-        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 20 }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center', gap: 20, justifyContent: 'space-between', marginTop: 20 }}>
+            <View style={{ flexDirection: "row", alignitems: 'center', gap: 10, justifycontent: 'center' }}>
+              <Text style={{ fontsize: 16, color: 'white' }}>
+                Cashback
+              </Text>
+              <EvilIcons name="question" size={24} color="black" />
+            </View>
 
-          <View style={{ height: 40, width: 40, backgroundColor: '#88888856', borderRadius: 5, justifyContent: 'center', alignItems: 'center' }}>
-            <Ionicons name="arrow-back" size={24} color="black" />
-
+            <View>
+              <Text style={{ fontsize: 16, color: 'white' }}>
+                Voucher
+              </Text>
+            </View>
 
           </View>
-          <Text style={{ fontSize: 22, fontWeight: '800' }}>Transfer Successful</Text>
-        </View>
 
-
-        <View style={{ marginTop: 40 }}>
-
-          <Text style={{ textAlign: 'justify', fontSize: 20 }}>
-            {"You transfer of ₦[amount] to [Recivers's Name]\nhas been completed successfully."}
-          </Text>
-
-          <View style={{ gap: 20, padding:20}}>
-
-            <Text style={{ fontSize: 20, fontWeight: '800', marginTop: 30 }}>Transaction Details</Text>
-
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-              <Text>Receiver</Text>
-              <Text>[Receiver's Name]</Text>
+          <View style={{ flexDirection: 'row', alignItems: 'center', gap: 20, justifyContent: 'space-between', marginTop: 20 }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center', gap: 10, justifycontent: 'center' }}>
+              <MaterialCommunityIcons name="bitcoin" size={24} color="black" />
+              <Text style={{ frontsize: 16, color: 'white' }}>
+                # 436.90
+              </Text>
             </View>
+            <View>
+              <Text style={{ fontsize: 16, color: 'white' }}>
+                #0
+              </Text>
 
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-              <Text>Account Number</Text>
-              <Text>[Account No.]</Text>
-            </View>
-
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-              <Text>Bank</Text>
-              <Text>[Bank Name]</Text>
-            </View>
-
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-              <Text>Receiver</Text>
-              <Text>#[amount]</Text>
-            </View>
-
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-              <Text>Date:</Text>
-              <Text>10/09/2025</Text>
-            </View>
-
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-              <Text>Time:</Text>
-              <Text>12:12pm</Text>
             </View>
           </View>
         </View>
 
-        <TouchableOpacity onPress={()=> setIsModalOn(true)} style={{ backgroundColor: 'black', justifyContent: 'center', alignItems: 'center', height: 50, width: 200, alignSelf: 'center', marginTop: 30, borderRadius: 10, flexDirection:'row', gap:'10' }}>
-          <MaterialCommunityIcons name="send" size={24} color="white" />
-          <Text style={{ color: 'white', fontSize: 20, fontWeight: '700' }}>Share Receipt</Text>
-        </TouchableOpacity>
 
+        <View style={{position:'relative', top:-50}}>
+          <FlatList
+            data={Data}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id.toString()}
+            horizontal
+          />
+        </View>
 
+        <Text style={{ fontsize: 16, color: 'white' }}>Daily Bonus</Text>
 
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={isModalOn}>
-          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}>
-            <View style={{ flexWrap: 'wrap', width: '65%', backgroundColor: 'white', borderRadius: 20, padding: 20, alignItems: 'center', justifyContent: 'center', alignContent: 'center' }}>
-
-              <View style={{ flexDirection: 'column' }}>
-
-                <TouchableOpacity onPress={() => setIsModalOn(false)} style={{ width: '100%', justifyContent: 'flex-end', paddingLeft: 220 }}>
-                  <MaterialIcons name="cancel" size={24} color="black" style={{}} />
-                </TouchableOpacity>
-
-                <View>
-                  <Text style={{ textAlign: 'center', fontSize: 22, fontWeight: "800" }}>
-                    Confirm Transfer
-                  </Text>
-                  <Text style={{ textAlign: 'center', fontSize: 12 }}>
-                    {"You are about to send\n₦(amount) to (Recivers's Name).\nPlease confirm the details before proceeding"}
-                  </Text>
-
-                  <View style={{ gap: 8 }}>
-
-                    <Text style={{ fontSize: 16, fontWeight: '800', marginTop: 30 }}>Transaction Details</Text>
-
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                      <Text>Receiver</Text>
-                      <Text>[Receiver's Name]</Text>
-                    </View>
-
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                      <Text>Account Number</Text>
-                      <Text>[Account No.]</Text>
-                    </View>
-
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                      <Text>Bank</Text>
-                      <Text>[Bank Name]</Text>
-                    </View>
-
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                      <Text>Receiver</Text>
-                      <Text>#[amount]</Text>
-                    </View>
-
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                      <Text>Date:</Text>
-                      <Text>10/09/2025</Text>
-                    </View>
-
-                  </View>
+        <View style={{ padding: 10, }}>
+          <View style={{ backgroundColor: 'gray', height: '70%', width: '100%', borderRadius: 15 }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center', justifycontent: 'center', gap: 10, }}>
+              <Text>Glo Airtime</Text>
+              <MaterialCommunityIcons name="bitcoin" size={24} color="black" />
+              <Text>+Up to 6%</Text>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center', justifycontent: 'center', gap: 10, }}>
+              <Text>Buy Airtime and get up to 6% cashback</Text>
+              <View>
+                <View style={{ height: 30, width: 60, alignItems: 'center', justifyContent: 'center', backgroundColor: 'green', borderRadius: 15 }}>
+                  <Text>Go</Text>
                 </View>
-
-                <TouchableOpacity style={{ backgroundColor: 'black', justifyContent: 'center', alignItems: 'center', height: 45, width: 200, alignSelf: 'center', marginTop: 30, borderRadius: 10 }}>
-                  <Text style={{ color: 'white', fontSize: 20, fontWeight: '700' }}>Contiune</Text>
-                </TouchableOpacity>
               </View>
-
-
-
-
-
             </View>
           </View>
-        </Modal>
+        </View>
 
       </SafeAreaView>
     </SafeAreaProvider>
@@ -152,8 +134,7 @@ export default function Rewards() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    padding: 20,
-    paddingVertical: 40
-  }
+    backgroundColor: '#000000',
+
+  },
 })
